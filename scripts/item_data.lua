b4f0f47100fcd68506b9af9ccfd81fb0dfc66636
Gearwcu <huizger@gmail.com>
2026-06-22 23:12:40 +0200

    Add files

---
+++ scripts/item_data.lua
@@ -0,0 +1,18 @@
+ITEM_GROUPS = {
+	["Bikes"] = {"progressive_bike"},
+	["Eternatus"] = {"broken_shards"},
+	["Weather"] = {"normal_weather", "overcast", "raining", "thunderstorm", "snowing", "snowstorm", "intense_sun", "sandstorm", "fog"},
+	["needsWeather"] = {"normal_weather", "overcast", "raining", "thunderstorm", "snowing", "snowstorm", "intense_sun", "sandstorm", "fog"},
+	["Locations"] = {"axews_eye", "bridge_field", "dappled_grove", "dusty_bowl", "east_lake_axewell", "giants_cap", "giants_mirror", "giants_seat", "hammerlocke_hills", "lake_of_outrage", "motostoke_riverbank", "north_lake_miloch", "south_lake_miloch", "stony_wilderness", "watchtower_ruins", "west_lake_axewell", "rolling_fields"},
+	["AllWeather"] = {"noweather"}
+}
+
+
+ITEM_VALUES = {
+
+}
+
+
+STAGE_MAPPINGS = {
+
+}

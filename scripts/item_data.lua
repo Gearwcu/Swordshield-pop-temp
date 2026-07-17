c32d9dcebb76f00bdc9a5d88a17a920df3910785
Gearwcu <huizger@gmail.com>
2026-07-17 14:02:35 +0200

    new

--- scripts/item_data.lua
+++ scripts/item_data.lua
@@ -1,10 +1,30 @@
 ITEM_GROUPS = {
 	["Bikes"] = {"progressive_bike"},
-	["Eternatus"] = {"broken_shards"},
+	["Broken shards"] = {"broken_shards"},
 	["Weather"] = {"normal_weather", "overcast", "raining", "thunderstorm", "snowing", "snowstorm", "intense_sun", "sandstorm", "fog"},
 	["needsWeather"] = {"normal_weather", "overcast", "raining", "thunderstorm", "snowing", "snowstorm", "intense_sun", "sandstorm", "fog"},
-	["Locations"] = {"axews_eye", "bridge_field", "dappled_grove", "dusty_bowl", "east_lake_axewell", "giants_cap", "giants_mirror", "giants_seat", "hammerlocke_hills", "lake_of_outrage", "motostoke_riverbank", "north_lake_miloch", "south_lake_miloch", "stony_wilderness", "watchtower_ruins", "west_lake_axewell", "rolling_fields"},
-	["AllWeather"] = {"noweather"}
+	["Wild Area Unlocks"] = {"axews_eye", "bridge_field", "dappled_grove", "dusty_bowl", "east_lake_axewell", "giants_cap", "giants_mirror", "giants_seat", "hammerlocke_hills", "lake_of_outrage", "motostoke_riverbank", "north_lake_miloch", "south_lake_miloch", "stony_wilderness", "watchtower_ruins", "west_lake_axewell", "rolling_fields"},
+	["AllWeather"] = {"noweather"},
+	["Type Collection"] = {"type_unlock_-_normal_type", "type_unlock_-_fire_type", "type_unlock_-_water_type", "type_unlock_-_grass_type", "type_unlock_-_electric_type", "type_unlock_-_ice_type", "type_unlock_-_fighting_type", "type_unlock_-_poison_type", "type_unlock_-_ground_type", "type_unlock_-_flying_type", "type_unlock_-_psychic_type", "type_unlock_-_bug_type", "type_unlock_-_rock_type", "type_unlock_-_ghost_type", "type_unlock_-_dragon_type", "type_unlock_-_dark_type", "type_unlock_-_steel_type", "type_unlock_-_fairy_type"},
+	["Normal"] = {"type_unlock_-_normal_type"},
+	["Fire"] = {"type_unlock_-_fire_type"},
+	["Water"] = {"type_unlock_-_water_type"},
+	["Grass"] = {"type_unlock_-_grass_type"},
+	["Electric"] = {"type_unlock_-_electric_type"},
+	["Ice"] = {"type_unlock_-_ice_type"},
+	["Fighting"] = {"type_unlock_-_fighting_type"},
+	["Poison"] = {"type_unlock_-_poison_type"},
+	["Ground"] = {"type_unlock_-_ground_type"},
+	["Flying"] = {"type_unlock_-_flying_type"},
+	["Psychic"] = {"type_unlock_-_psychic_type"},
+	["Bug"] = {"type_unlock_-_bug_type"},
+	["Rock"] = {"type_unlock_-_rock_type"},
+	["Ghost"] = {"type_unlock_-_ghost_type"},
+	["Dragon"] = {"type_unlock_-_dragon_type"},
+	["Dark"] = {"type_unlock_-_dark_type"},
+	["Steel"] = {"type_unlock_-_steel_type"},
+	["Fairy"] = {"type_unlock_-_fairy_type"},
+	["Location Unlocks"] = {"route_1_access", "route_2_access", "route_3_access", "route_4_access", "route_5_access", "route_6_access", "route_7_access", "route_8_access", "route_9_access", "route_10_access", "galar_mine_access", "galar_mine_2_access", "slumbering_weald_access", "glimwood_tangle_access", "motostoke_outskirts_access"}
 }
 
 
